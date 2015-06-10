6784ab2c0517e9a44ec721f504c9f457c6c56a72
Jaroslaw Pogoda <jarek1330@gmail.com>
2015-06-10 12:33:20 -0400

    Wrevel Project In angularjs
    Javascript app using angular javascript

---
+++ scripts.js
@@ -0,0 +1,36 @@
+function billsfromwrevel($scope){
+	$scope.payments = [
+		{
+			name: 'Web Development',
+			price: 300,
+			active:false
+		},
+		{
+			name: 'Design',
+			price: 400,
+			active:false
+		},
+		{
+			name: 'Integration',
+			price: 250,
+			active:false
+		},
+		{
+			name: 'Training',
+			price: 220,
+			active:false
+		}
+	];
+	$scope.toggleActive = function(s){
+		s.active = !s.active;
+	};
+	$scope.total = function(){
+		var total = 0;
+		angular.forEach($scope.payments, function(s){
+			if (s.active){
+				total+= s.price;
+			}
+		});
+		return total;
+	};
+}
